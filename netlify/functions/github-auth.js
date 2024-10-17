d4cb1ff8305ba28321a08e23a661a194c4cb1cc6
Michael T <NetShadowM@proton.me>
2024-10-17 09:47:05 +0100

    added admin login and authentication

---
+++ netlify/functions/github-auth.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+    const { code } = JSON.parse(event.body);
+
+    try {
+        const response = await axios.post('https://github.com/login/oauth/access_token', {
+            client_id: process.env.GITHUB_CLIENT_ID,
+            client_secret: process.env.GITHUB_CLIENT_SECRET,
+            code,
+        }, {
+            headers: { 'Accept': 'application/json' },
+        });
+
+        const { access_token } = response.data;
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ access_token }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'OAuth token exchange failed' }),
+        };
+    }
+};
